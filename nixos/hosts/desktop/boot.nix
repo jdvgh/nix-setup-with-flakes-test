67492c9d1f3a2c5a61745314ae17570ca1650065
Eilers, Jonas <133217951+jdvgh@users.noreply.github.com>
2024-02-10 13:31:47 +0100

    feat: Add correct luks entry in boot setup for dekstop

--- nixos/hosts/desktop/boot.nix
+++ nixos/hosts/desktop/boot.nix
@@ -13,5 +13,6 @@
         canTouchEfiVariables = true;
       };
     };
+  initrd.luks.devices."luks-99f1418e-d80c-4bc5-a9d3-3d04b545e0e4".device = "/dev/disk/by-uuid/99f1418e-d80c-4bc5-a9d3-3d04b545e0e4";
   };
 }
